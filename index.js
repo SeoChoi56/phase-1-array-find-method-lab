52d63b95cc025e999095cb00d65b927dfa9ee581
Seo Choi <seochoi56@gmail.com>
2022-08-30 16:02:08 -0400

    commit tree 4cbf55243828ad3d6c9f16c112491149de64f957

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+//code your solution here
+
+function isWin(element){
+    return element.result === "W"
+}
+
+
+function superbowlWin(record) {
+    const element = record.find(isWin)
+    if(element)
+    return element.year;
+    else 
+    return undefined;
+}
